d1d6c5ddaefbe4d660fa8ae4eadc36309ab7dc37
Berke Sayın <sayinberke34@gmail.com>
2025-01-06 14:29:09 +0300

    Use open feign to send sync request to basket service from payment service

---
+++ services/payment/src/main/java/dev/berke/app/basket/BasketClient.java
@@ -0,0 +1,18 @@
+package dev.berke.app.basket;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "basket-service",
+        url = "${application.config.basket-url}"
+)
+public interface BasketClient {
+
+    @GetMapping("/{customer-id}")
+    BasketResponse getBasketByCustomerId(@PathVariable("customer-id") String customerId);
+
+    @GetMapping("/{customer-id}/total-price")
+    BasketTotalPriceResponse getTotalBasketPrice(@PathVariable("customer-id") String customerId);
+}
