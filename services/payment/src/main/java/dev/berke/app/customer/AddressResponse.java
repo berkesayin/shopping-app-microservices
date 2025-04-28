bf28dcd90e26313f9535f75ce4a0ae066f553810
Berke Sayın <sayinberke34@gmail.com>
2025-04-28 19:26:31 +0300

    Update payment details for new customer domain

---
+++ services/payment/src/main/java/dev/berke/app/customer/AddressResponse.java
@@ -0,0 +1,12 @@
+package dev.berke.app.customer;
+
+public record AddressResponse(
+        String id,
+        String contactName,
+        String city,
+        String country,
+        String address,
+        String zipCode,
+        Boolean isActive
+) {
+}
