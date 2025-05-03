926150c79904d05b57efc4990251dcca3f2c38ff
Berke Sayın <sayinberke34@gmail.com>
2025-05-03 19:37:40 +0300

    Update product and category models

--- services/product/src/main/java/dev/berke/app/product/ProductRepository.java
+++ services/product/src/main/java/dev/berke/app/product/ProductRepository.java
@@ -2,9 +2,5 @@ package dev.berke.app.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-    List<Product> findAllByIdInOrderById(List<Integer> productIds);
 }
