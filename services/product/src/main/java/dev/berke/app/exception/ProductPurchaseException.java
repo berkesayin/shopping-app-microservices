17a8cb8bd4c8a5f1722228074ae67bfefcf2b407
Berke Sayın <sayinberke34@gmail.com>
2025-05-03 19:31:18 +0300

    Delete unused commands, dtos and methods after updated product model

--- services/product/src/main/java/dev/berke/app/exception/ProductPurchaseException.java
+++
@@ -1,8 +0,0 @@
-package dev.berke.app.exception;
-
-public class ProductPurchaseException extends RuntimeException {
-
-    public ProductPurchaseException(String message) {
-        super(message);
-    }
-}
