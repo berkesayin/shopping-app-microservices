8e84215dcaf453bb82327d3e15e61edf623bc0ed
Berke Sayın <sayinberke34@gmail.com>
2025-05-11 19:02:50 +0300

    Refactor files and namings

--- services/order/src/main/java/dev/berke/app/product/ProductClient.java
+++
@@ -1,8 +0,0 @@
-package dev.berke.app.product;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "product-service", url = "${application.config.product-url}")
-public interface ProductClient {
-
-}
